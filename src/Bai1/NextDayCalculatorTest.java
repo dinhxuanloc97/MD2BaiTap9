28f967ed7ef017cc375ceda700782ce9bbfb122f
dinhxuanloc97 <dinhxuanloc741997@gmail.com>
2022-10-07 14:44:21 +0700

    len

---
+++ src/Bai1/NextDayCalculatorTest.java
@@ -0,0 +1,22 @@
+package Bai1;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NextDayCalculatorTest {
+
+    @Test
+    void getNextDayTest1() {
+        //Input
+        int day = 28;
+        int month=2;
+        int year =2022;
+        //Ket qua mong muon
+        String excpected = "1 / 3 / 2022";
+        //Ket qua thuc te
+        NextDayCalculator ndc = new NextDayCalculator();
+        String strActual = ndc.getNextDay(day,month,year);
+        //So sanh
+        assertEquals(excpected,strActual);
+    }
+}
